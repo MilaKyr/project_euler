83afacb49d51b92457105b60aef706329969aba6
MilaKyr <kirashchukmila@gmail.com>
2021-03-29 22:51:28 +0200

    Fix notations

--- problems/prb_1.py
+++ problems/prb_1.py
@@ -1,4 +1,3 @@
-import sys
 """
 Multiples of 3 and 5
 
@@ -6,16 +5,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
+import sys
 
 
-def multiples(num: int):
-    return sum([v for v in range(1, num) if v % 5 == 0 or v % 3 == 0])
+def sum_of_5_and_3_multiples(num: int):
+    return sum([n for n in range(1, num) if n % 5 == 0 or n % 3 == 0])
 
 
 if __name__ == "__main__":
     try:
         num = int(sys.argv[1])
-        print(f"The sum of all the multiples of 3 or 5 below {num} is {multiples(num)}")
+        print(f"The sum of all the multiples of 3 or 5 below {num} is {sum_of_5_and_3_multiples(num)}")
     except Exception as e:
         print(e)
 
